610257e634e288953ac9bf96ada7a7616dee5c04
Julian Rüth <julian.rueth@fsfe.org>
2021-04-16 21:20:41 +0200

    remove sysroot for Python on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,10 +9,6 @@ if [[ $(uname) == 'Darwin' ]] && [[ $PY3K == 1 ]]; then
     export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
 fi
 
-# Disable -isysroot magic that breaks the build:
-# clang-11: warning: no such sysroot directory: '/Applications/Xcode_12.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk -march=core2 -mtune=haswell -mssse3 -ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe -isystem /Users/runner/miniforge3/conda-bld/cryptominisat_1618595078418/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_place/include -fdebug-prefix-map=/Users/runner/miniforge3/conda-bld/cryptominisat_1618595078418/work=/usr/local/src/conda/cryptominisat-5.8.0 -fdebug-prefix-map=/Users/runner/miniforge3/conda-bld/cryptominisat_1618595078418/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_place=/usr/local/src/conda-prefix' [-Wmissing-sysroot]
-export NIX_CC=yes
-
 # * ENABLE_TESTING: Testing requires lit which is not packaged yet:
 #   https://github.com/conda-forge/staged-recipes/issues/4630
 cmake \
